1fb50035e409d395ecf61c5695978be1af9f63f7
Rodrigo Melo <rodrigomelo01@gmail.com>
2016-05-18 07:35:45 -0300

    Finalização da aula 2.10

--- src/main/java/com/algaworks/socialbooks/resources/LivrosResources.java
+++ src/main/java/com/algaworks/socialbooks/resources/LivrosResources.java
@@ -1,13 +1,17 @@
 package com.algaworks.socialbooks.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.algaworks.socialbooks.domain.Livro;
 import com.algaworks.socialbooks.repository.LivrosRepository;
@@ -25,13 +29,28 @@ public class LivrosResources {
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
-	public void salvar(@RequestBody Livro livro) {
-		livrosRepository.save(livro);
+	public ResponseEntity<Void> salvar(@RequestBody Livro livro) {
+		//O método save sempre retorna o objeto salvo. 
+		livro = livrosRepository.save(livro);
+		
+		// Monta o URI que será retornada para o cliente, no cabeçalho HTTP. 
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(livro.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public Livro buscar(@PathVariable("id") Long id){
-		return livrosRepository.findOne(id);
+	public ResponseEntity<?> buscar(@PathVariable("id") Long id){
+		//A classe ResponseEntity é um builder que permite retornar respostas
+		//ao cliente para seu devido tratamento.
+		Livro livro = livrosRepository.findOne(id);
+		
+		if (livro == null) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		return ResponseEntity.status(HttpStatus.OK).body(livro);
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
