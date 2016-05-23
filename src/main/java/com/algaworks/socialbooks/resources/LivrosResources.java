e67bd664b1607810710641e24d100c0093c2dca5
Rodrigo Melo <rodrigomelo01@gmail.com>
2016-05-22 22:21:19 -0300

    Fim da aula 2.14.

--- src/main/java/com/algaworks/socialbooks/resources/LivrosResources.java
+++ src/main/java/com/algaworks/socialbooks/resources/LivrosResources.java
@@ -4,7 +4,6 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,9 +13,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.algaworks.socialbooks.domain.Comentario;
 import com.algaworks.socialbooks.domain.Livro;
 import com.algaworks.socialbooks.services.LivrosService;
-import com.algaworks.socialbooks.services.exceptions.LivroNaoEncontradoException;
 
 @RestController
 @RequestMapping("/livros")
@@ -27,48 +26,59 @@ public class LivrosResources {
 
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<Livro>> listar() {
-		
+
 		return ResponseEntity.status(HttpStatus.OK).body(livrosService.listar());
 	}
-	
+
 	@RequestMapping(method = RequestMethod.POST)
 	public ResponseEntity<Void> salvar(@RequestBody Livro livro) {
-		//O método save sempre retorna o objeto salvo. 
+		// O método save sempre retorna o objeto salvo.
 		livro = livrosService.salvar(livro);
-		
-		// Monta o URI que será retornada para o cliente, no cabeçalho HTTP. 
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
-				.path("/{id}")
-				.buildAndExpand(livro.getId())
-				.toUri();
-		
+
+		// Monta o URI que será retornada para o cliente, no cabeçalho HTTP.
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(livro.getId()).toUri();
+
 		return ResponseEntity.created(uri).build();
 	}
-	
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public ResponseEntity<?> buscar(@PathVariable("id") Long id){
-		//A classe ResponseEntity é um builder que permite retornar respostas
-		//ao cliente para seu devido tratamento.
-		Livro livro = livrosService.buscar(id);;
-		
+	public ResponseEntity<?> buscar(@PathVariable("id") Long id) {
+		// A classe ResponseEntity é um builder que permite retornar respostas
+		// ao cliente para seu devido tratamento.
+		Livro livro = livrosService.buscar(id);
+		;
+
 		return ResponseEntity.status(HttpStatus.OK).body(livro);
 	}
-	
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-	public ResponseEntity<Void> deletar(@PathVariable("id") Long id){
-		
+	public ResponseEntity<Void> deletar(@PathVariable("id") Long id) {
+
 		livrosService.deletar(id);
-		
-		//Retorna a resposta 204, mostrando que tudo foi processado normalmente e
-		//que nada foi retornado.
+
+		// Retorna a resposta 204, mostrando que tudo foi processado normalmente
+		// e
+		// que nada foi retornado.
 		return ResponseEntity.noContent().build();
 	}
-	
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-	public ResponseEntity<Void> atualizar(@RequestBody Livro livro, @PathVariable("id") Long id){
+	public ResponseEntity<Void> atualizar(@RequestBody Livro livro, @PathVariable("id") Long id) {
 		livro.setId(id);
 		livrosService.salvar(livro);
-				
+
 		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(value = "/{id}/comentarios", method = RequestMethod.POST)
+	public ResponseEntity<Void> adicionarComentario(@PathVariable("id") Long livroId, @RequestBody Comentario comentario) {
+		
+		livrosService.salvarComentario(livroId, comentario);
+		
+		//Cria a URI que será retornada ao cliente. Neste caso serão retornados todos os comentários.
+		//Portanto, não há a necessidade de passar um id como parâmetro.
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().build().toUri();
+		
+		return ResponseEntity.created(uri).build();
 	}
 }
