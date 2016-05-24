13639c0e41133d2e3db2ffce3c6c103ee3e264c0
Rodrigo Melo <rodrigomelo01@gmail.com>
2016-05-24 07:39:19 -0300

    Fim da aula 2.16 do curso de Spring Rest.

---
+++ src/main/java/com/algaworks/socialbooks/resources/AutoresResources.java
@@ -0,0 +1,30 @@
+package com.algaworks.socialbooks.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.algaworks.socialbooks.domain.Autor;
+import com.algaworks.socialbooks.domain.Livro;
+import com.algaworks.socialbooks.services.AutoresService;
+
+@RestController
+@RequestMapping("/autores")
+public class AutoresResources {
+
+	@Autowired
+	private AutoresService autoresServices;
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<Autor>> listar(){
+		
+		List<Autor> autores = autoresServices.listar();
+		
+		return ResponseEntity.status(HttpStatus.OK).body(autores);
+	}
+}
