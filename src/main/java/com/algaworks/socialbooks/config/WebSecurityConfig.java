0208ffcf73def0377d28ef8cd99e4900e3683f81
Rodrigo Melo <rodrigomelo01@gmail.com>
2016-05-28 23:12:41 -0300

    Fim da aula 3.2 de Spring REST.

---
+++ src/main/java/com/algaworks/socialbooks/config/WebSecurityConfig.java
@@ -0,0 +1,30 @@
+package com.algaworks.socialbooks.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+		//Configura uma autenticação global em memória.
+		auth.inMemoryAuthentication().withUser("algaworks")
+		.password("s3nh4").roles("USER");
+	}
+	
+	protected void configure(HttpSecurity http) throws Exception{
+		
+		http.
+			authorizeRequests()
+			.antMatchers("/h2-console/**").permitAll()
+			.anyRequest().authenticated() //Todas as requisições terão que estar autenticadas.
+			.and()
+				.httpBasic() //Pela autenticação básica do HTTP.
+			.and()
+				.csrf().disable();//E com a definição de certificado desabilitada, pois a API será pública.
+	}
+}
